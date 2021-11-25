3dafce82c6abff980d75b9011fc3f1c39c5fad9a
sebvil <8863909+sebvil@users.noreply.github.com>
2021-11-25 16:58:41 -0600

    progress

--- app/src/test/java/com/sebastianvm/musicplayer/ui/library/albums/AlbumsListViewModelTest.kt
+++ app/src/test/java/com/sebastianvm/musicplayer/ui/library/albums/AlbumsListViewModelTest.kt
@@ -1,19 +1,38 @@
 package com.sebastianvm.musicplayer.ui.library.albums
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.mockito.kotlin.mock
 
 class AlbumsListViewModelTest {
 
 
-    fun generateViewModel(): AlbumsListViewModel {
+    private     fun generateViewModel(): AlbumsListViewModel {
         return AlbumsListViewModel(mock(), mock())
     }
 
+//    @OptIn(ExperimentalCoroutinesApi::class)
+//    private val testDispatcher = StandardTestDispatcher()
+
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `trial test`(){
+        Dispatchers.setMain(Dispatchers.Unconfined)
         with(generateViewModel()) {
             handle(AlbumsListUserAction.AlbumClicked("a","a"))
+            CoroutineScope(Dispatchers.Main).launch {
+                eventsFlow.collect {
+                    assertTrue(it is AlbumsListUiEvent.NavigateToAlbum)
+                    assertTrue(false)
+                }
+            }
         }
     }
 }
