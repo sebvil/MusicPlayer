eadefdb7d7ea11692c3f9e641bffa5d40363cc12
sebvil <8863909+sebvil@users.noreply.github.com>
2021-11-25 16:38:59 -0600

    replaced state with stateflow

--- app/src/main/java/com/sebastianvm/musicplayer/ui/util/compose/Screen.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/ui/util/compose/Screen.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import com.sebastianvm.musicplayer.ui.util.mvvm.BaseViewModel
 import com.sebastianvm.musicplayer.ui.util.mvvm.UserAction
@@ -13,17 +13,18 @@ import com.sebastianvm.musicplayer.ui.util.mvvm.events.EventHandler
 import com.sebastianvm.musicplayer.ui.util.mvvm.events.HandleEvents
 import com.sebastianvm.musicplayer.ui.util.mvvm.events.UiEvent
 import com.sebastianvm.musicplayer.ui.util.mvvm.state.State
+import kotlinx.coroutines.Dispatchers
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun <A: UserAction, E: UiEvent, S: State> Screen(
-    screenViewModel: BaseViewModel<A,E,S>,
+fun <A : UserAction, E : UiEvent, S : State> Screen(
+    screenViewModel: BaseViewModel<A, E, S>,
     eventHandler: EventHandler<E>,
     topBar: @Composable (S) -> Unit = {},
     fab: @Composable (S) -> Unit = {},
     content: @Composable (S) -> Unit
 ) {
-    val state = screenViewModel.state.observeAsState(initial = screenViewModel.state.value)
+    val state = screenViewModel.state.collectAsState(context = Dispatchers.Main)
     HandleEvents(eventsFlow = screenViewModel.eventsFlow, eventHandler = eventHandler)
     Scaffold(
         topBar = { topBar(state.value) },
@@ -33,5 +34,4 @@ fun <A: UserAction, E: UiEvent, S: State> Screen(
             content(state.value)
         }
     }
-
 }
