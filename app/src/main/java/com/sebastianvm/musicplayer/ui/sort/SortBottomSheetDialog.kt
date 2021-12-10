644df97476c2d0c2ca1931c4dcff06fc0a7d2f63
sebvil <8863909+sebvil@users.noreply.github.com>
2021-12-09 23:23:43 -0600

    fix colors

--- app/src/main/java/com/sebastianvm/musicplayer/ui/sort/SortBottomSheetDialog.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/ui/sort/SortBottomSheetDialog.kt
@@ -86,9 +86,10 @@ fun SortBottomSheet(
                         Icon(
                             painter = painterResource(id = if (state.value.sortOrder == SortOrder.ASCENDING) R.drawable.ic_up else R.drawable.ic_down),
                             contentDescription = "temp",
-                            modifier = Modifier.padding(end = AppDimensions.spacing.large)
+                            modifier = Modifier.padding(end = AppDimensions.spacing.large),
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant
                         )
-                        Text(text = stringResource(id = row), modifier = Modifier.weight(1f))
+                        Text(text = stringResource(id = row), modifier = Modifier.weight(1f), color = MaterialTheme.colorScheme.onSurfaceVariant)
                     } else {
                         Text(
                             text = stringResource(id = row),
