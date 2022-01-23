ac0d793b85d6e86ba701b74d431121c6e7852ad2
sebvil <8863909+sebvil@users.noreply.github.com>
2022-01-22 19:35:34 -0600

    Use kotlin test coroutines utilities and mockk to get rid of Robolectric in tests

--- app/src/test/java/com/sebastianvm/musicplayer/ui/artist/ArtistViewModelTest.kt
+++ app/src/test/java/com/sebastianvm/musicplayer/ui/artist/ArtistViewModelTest.kt
@@ -1,7 +1,7 @@
 package com.sebastianvm.musicplayer.ui.artist
 
 import android.content.ContentUris
-import android.provider.MediaStore
+import android.net.Uri
 import com.sebastianvm.commons.R
 import com.sebastianvm.musicplayer.database.entities.AlbumBuilder
 import com.sebastianvm.musicplayer.database.entities.ArtistBuilder
@@ -12,22 +12,44 @@ import com.sebastianvm.musicplayer.repository.artist.FakeArtistRepository
 import com.sebastianvm.musicplayer.ui.artist.ArtistViewModel.Companion.ALBUMS
 import com.sebastianvm.musicplayer.ui.artist.ArtistViewModel.Companion.APPEARS_ON
 import com.sebastianvm.musicplayer.ui.components.AlbumRowState
+import com.sebastianvm.musicplayer.util.DispatcherSetUpRule
 import com.sebastianvm.musicplayer.util.expectUiEvent
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
 
-@RunWith(RobolectricTestRunner::class)
 class ArtistViewModelTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @get:Rule
+    val mainCoroutineRule = DispatcherSetUpRule()
 
     private val albumRepository: AlbumRepository = FakeAlbumRepository()
     private val artistRepository: ArtistRepository = FakeArtistRepository()
+
+    private lateinit var defaultUri: Uri
+    private lateinit var secondaryUri: Uri
+
+    @Before
+    fun setUp() {
+        mockkStatic(ContentUris::class)
+        defaultUri = mockk()
+        secondaryUri = mockk()
+        every {
+            ContentUris.withAppendedId(any(), AlbumBuilder.DEFAULT_ALBUM_ID.toLong())
+        } returns defaultUri
+        every {
+            ContentUris.withAppendedId(any(), AlbumBuilder.SECONDARY_ALBUM_ID.toLong())
+        } returns secondaryUri
+    }
 
     private fun generateViewModel(): ArtistViewModel {
         return ArtistViewModel(
@@ -45,45 +67,36 @@ class ArtistViewModelTest {
     @Test
     fun `init sets initial state values`() = runTest {
         with(generateViewModel()) {
-            launch {
-                assertEquals(
-                    listOf(
-                        ArtistScreenItem.SectionHeaderItem(ALBUMS, R.string.albums),
-                        ArtistScreenItem.AlbumRowItem(
-                            AlbumRowState(
-                                albumId = AlbumBuilder.DEFAULT_ALBUM_ID,
-                                albumName = AlbumBuilder.DEFAULT_ALBUM_NAME,
-                                imageUri =
-                                ContentUris.withAppendedId(
-                                    MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
-                                    AlbumBuilder.DEFAULT_ALBUM_ID.toLong()
-                                ),
-                                year = AlbumBuilder.DEFAULT_YEAR,
-                                artists = ArtistBuilder.DEFAULT_ARTIST_NAME
-                            )
-                        )
-                    ), state.value.albumsForArtistItems
-                )
-                assertEquals(
-                    listOf(
-                        ArtistScreenItem.SectionHeaderItem(APPEARS_ON, R.string.appears_on),
-                        ArtistScreenItem.AlbumRowItem(
-                            AlbumRowState(
-                                albumId = AlbumBuilder.SECONDARY_ALBUM_ID,
-                                albumName = AlbumBuilder.SECONDARY_ALBUM_NAME,
-                                imageUri =
-                                ContentUris.withAppendedId(
-                                    MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
-                                    AlbumBuilder.SECONDARY_ALBUM_ID.toLong()
-                                ),
-                                year = AlbumBuilder.SECONDARY_YEAR,
-                                artists = ArtistBuilder.SECONDARY_ARTIST_NAME
-                            )
-                        )
-                    ), state.value.appearsOnForArtistItems
-                )
-            }
             delay(1)
+            assertEquals(
+                listOf(
+                    ArtistScreenItem.SectionHeaderItem(ALBUMS, R.string.albums),
+                    ArtistScreenItem.AlbumRowItem(
+                        AlbumRowState(
+                            albumId = AlbumBuilder.DEFAULT_ALBUM_ID,
+                            albumName = AlbumBuilder.DEFAULT_ALBUM_NAME,
+                            imageUri = defaultUri,
+                            year = AlbumBuilder.DEFAULT_YEAR,
+                            artists = ArtistBuilder.DEFAULT_ARTIST_NAME
+                        )
+                    )
+                ), state.value.albumsForArtistItems
+            )
+            assertEquals(
+                listOf(
+                    ArtistScreenItem.SectionHeaderItem(APPEARS_ON, R.string.appears_on),
+                    ArtistScreenItem.AlbumRowItem(
+                        AlbumRowState(
+                            albumId = AlbumBuilder.SECONDARY_ALBUM_ID,
+                            albumName = AlbumBuilder.SECONDARY_ALBUM_NAME,
+                            imageUri = secondaryUri,
+                            year = AlbumBuilder.SECONDARY_YEAR,
+                            artists = ArtistBuilder.SECONDARY_ARTIST_NAME
+                        )
+                    )
+                ), state.value.appearsOnForArtistItems
+            )
+
         }
     }
 
