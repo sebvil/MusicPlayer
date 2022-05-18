62c27f06fc89f29fb9814e73051c6d547c919a9a
sebvil <>
2022-05-17 19:01:28 -0500

    Small cleanups

--- app/src/main/java/com/sebastianvm/musicplayer/ui/components/chip/FilterChipGroup.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/ui/components/chip/FilterChipGroup.kt
@@ -1,14 +1,17 @@
 package com.sebastianvm.musicplayer.ui.components.chip
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.sebastianvm.musicplayer.ui.util.compose.AppDimensions
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun <T> SingleSelectFilterChipGroup(
     options: List<T>,
@@ -25,8 +28,9 @@ fun <T> SingleSelectFilterChipGroup(
         items(options) { option ->
             FilterChip(
                 selected = selectedOption == option,
-                text = option.getDisplayName(),
-                modifier = Modifier.clickable { onNewOptionSelected(option) })
+                onClick = { onNewOptionSelected(option) },
+                label = { Text(text = option.getDisplayName()) },
+            )
         }
     }
 }
