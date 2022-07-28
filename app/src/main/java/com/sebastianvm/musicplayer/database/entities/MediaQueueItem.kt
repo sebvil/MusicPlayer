ba6e47fb2c11faffe9ba2067d88dedffe2cde929
sebvil <>
2022-07-27 20:59:27 -0700

    updated room

--- app/src/main/java/com/sebastianvm/musicplayer/database/entities/MediaQueueItem.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/database/entities/MediaQueueItem.kt
@@ -3,7 +3,7 @@ package com.sebastianvm.musicplayer.database.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
-import androidx.room.ForeignKey.CASCADE
+import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
 
 @Entity(
