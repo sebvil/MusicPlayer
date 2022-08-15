cdb120e07e36f25ed6cbe6ee769abfaba05640bd
sebvil <>
2022-08-15 11:01:03 -0500

    Use ModeListState for playlists

--- app/src/main/java/com/sebastianvm/musicplayer/ui/library/playlists/PlaylistsListScreenPreviewParametersProvider.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/ui/library/playlists/PlaylistsListScreenPreviewParametersProvider.kt
@@ -1,7 +1,7 @@
 package com.sebastianvm.musicplayer.ui.library.playlists
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import com.sebastianvm.musicplayer.database.entities.Playlist
+import com.sebastianvm.musicplayer.ui.components.lists.ModelListItemState
 import com.sebastianvm.musicplayer.util.sort.MediaSortOrder
 
 class PlaylistsListStatePreviewParameterProvider :
@@ -9,20 +9,20 @@ class PlaylistsListStatePreviewParameterProvider :
     override val values = sequenceOf(
         PlaylistsListState(
             playlistsList = listOf(
-                Playlist(id = 0, "Pop"),
-                Playlist(id = 1, "Rock"),
-                Playlist(id = 2, "Tropipop"),
-                Playlist(id = 3, "Vallenato")
+                ModelListItemState(id = 0, "Pop"),
+                ModelListItemState(id = 1, "Rock"),
+                ModelListItemState(id = 2, "Tropipop"),
+                ModelListItemState(id = 3, "Vallenato")
             ),
             sortOrder = MediaSortOrder.ASCENDING,
             isDialogOpen = false,
         ),
         PlaylistsListState(
             playlistsList = listOf(
-                Playlist(id = 0, "Pop"),
-                Playlist(id = 1, "Rock"),
-                Playlist(id = 2, "Tropipop"),
-                Playlist(id = 3, "Vallenato")
+                ModelListItemState(id = 0, "Pop"),
+                ModelListItemState(id = 1, "Rock"),
+                ModelListItemState(id = 2, "Tropipop"),
+                ModelListItemState(id = 3, "Vallenato")
             ),
             sortOrder = MediaSortOrder.ASCENDING,
             isDialogOpen = true,
