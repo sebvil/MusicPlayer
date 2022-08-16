dffadc6516311164fe072ae7806f8957261009f5
sebvil <>
2022-08-15 23:18:02 -0500

    Hold results ina single list in search

--- app/src/main/java/com/sebastianvm/musicplayer/database/daos/ArtistFtsDao.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/database/daos/ArtistFtsDao.kt
@@ -1,14 +1,16 @@
 package com.sebastianvm.musicplayer.database.daos
 
-import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Query
 import com.sebastianvm.musicplayer.database.entities.Artist
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ArtistFtsDao {
-    @Query("SELECT * FROM Artist " +
-            "JOIN ArtistFts ON Artist.artistName == ArtistFts.artistName " +
-            "WHERE ArtistFts.artistName MATCH :text" )
-    fun artistsWithText(text: String): PagingSource<Int, Artist>
+    @Query(
+        "SELECT * FROM Artist " +
+                "JOIN ArtistFts ON Artist.artistName == ArtistFts.artistName " +
+                "WHERE ArtistFts.artistName MATCH :text"
+    )
+    fun artistsWithText(text: String): Flow<List<Artist>>
 }
--- app/src/main/java/com/sebastianvm/musicplayer/database/daos/GenreFtsDao.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/database/daos/GenreFtsDao.kt
@@ -1,12 +1,12 @@
 package com.sebastianvm.musicplayer.database.daos
 
-import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Query
 import com.sebastianvm.musicplayer.database.entities.Genre
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GenreFtsDao {
-    @Query("SELECT * FROM Genre JOIN GenreFts ON Genre.genreName == GenreFts.genreName WHERE GenreFts.genreName MATCH :text" )
-    fun genresWithText(text: String): PagingSource<Int, Genre>
+    @Query("SELECT * FROM Genre JOIN GenreFts ON Genre.genreName == GenreFts.genreName WHERE GenreFts.genreName MATCH :text")
+    fun genresWithText(text: String): Flow<List<Genre>>
 }
