df0c9e4d3507daa0d54d2e6dde323eb19f98ae5f
sebvil <>
2022-08-19 16:07:01 -0700

    Fixed tests for MusicPlayerViewModel

--- app/src/test/java/com/sebastianvm/musicplayer/ui/player/MusicPlayerViewModelTest.kt
+++ app/src/test/java/com/sebastianvm/musicplayer/ui/player/MusicPlayerViewModelTest.kt
@@ -1,121 +1,134 @@
-//package com.sebastianvm.musicplayer.ui.player
-//
-//import android.net.Uri
-//import com.sebastianvm.musicplayer.repository.playback.FakePlaybackManager
-//import com.sebastianvm.musicplayer.repository.playback.MediaItemMetadata
-//import com.sebastianvm.musicplayer.repository.playback.PlaybackManager
-//import com.sebastianvm.musicplayer.repository.playback.PlaybackState
-//import com.sebastianvm.musicplayer.util.DispatcherSetUpRule
-//import io.mockk.spyk
-//import io.mockk.verify
-//import kotlinx.coroutines.ExperimentalCoroutinesApi
-//import kotlinx.coroutines.test.runTest
-//import org.junit.Assert.assertEquals
-//import org.junit.Rule
-//import org.junit.Test
-//
-//class MusicPlayerViewModelTest : BaseTest() {
-//
-//    @OptIn(ExperimentalCoroutinesApi::class)
-//    @get:Rule
-//    val dispatcherSetUpRule = DispatcherSetUpRule()
-//
-//    private lateinit var playbackManager: PlaybackManager
-//
-//
-//    private fun generateViewModel(isPlaying: Boolean = false): MusicPlayerViewModel {
-//        playbackManager = spyk(
-//            FakePlaybackManager(
-//                playbackState = PlaybackState(
-//                    mediaItemMetadata = MediaItemMetadata(
-//                        title = TRACK_NAME,
-//                        artists = ARTIST_NAME,
-//                        artworkUri = ARTWORK_URI,
-//                        trackDurationMs = TRACK_LENGTH
-//                    ),
-//                    isPlaying = isPlaying,
-//                    currentPlayTimeMs = 0L
-//                )
-//            )
-//        )
-//        return MusicPlayerViewModel(
-//            mediaPlaybackRepository = playbackManager,
-//            initialState = MusicPlayerState(
-//                isPlaying = false,
-//                trackName = null,
-//                artists = null,
-//                trackLengthMs = null,
-//                currentPlaybackTimeMs = null,
-//                trackArt = Uri.EMPTY,
-//            )
-//        )
-//    }
-//
-//    @OptIn(ExperimentalCoroutinesApi::class)
-//    @Test
-//    fun `init sets initial state`() = testScope.runReliableTest {
-//        with(generateViewModel()) {
-//            assertEquals(
-//                MusicPlayerState(
-//                    trackName = TRACK_NAME,
-//                    artists = ARTIST_NAME,
-//                    trackArt = ARTWORK_URI,
-//                    trackLengthMs = TRACK_LENGTH,
-//                    isPlaying = false,
-//                    currentPlaybackTimeMs = 0,
-//                    ), state.value
-//            )
-//        }
-//    }
-//
-//
-//    @Test
-//    fun `onPlayToggled while paused triggers playback`() {
-//        with(generateViewModel()) {
-//            onPlayToggled()
-//            verify { playbackManager.play() }
-//        }
-//    }
-//
-//    @Test
-//    fun `onPlayToggled while playing pauses playback`() {
-//        with(generateViewModel(isPlaying = true)) {
-//            onPlayToggled()
-//            verify { playbackManager.pause() }
-//        }
-//    }
-//
-//
-//    @Test
-//    fun `onPreviousTapped triggers prev call`() {
-//        with(generateViewModel()) {
-//            onPreviousTapped()
-//            verify { playbackManager.prev() }
-//        }
-//    }
-//
-//    @Test
-//    fun `onNextTapped triggers next call`() {
-//        with(generateViewModel()) {
-//            onNextTapped()
-//            verify { playbackManager.next() }
-//        }
-//    }
-//
-//    @Test
-//    fun `onProgressTapped triggers seekToTrackPosition call`() {
-//        with(generateViewModel()) {
-//            onProgressTapped(50)
-//            verify { playbackManager.seekToTrackPosition(TRACK_LENGTH / 2) }
-//        }
-//    }
-//
-//    companion object {
-//        private const val TRACK_NAME = "TRACK_NAME"
-//        private const val ARTIST_NAME = "ARTIST_NAME"
-//        private val ARTWORK_URI = Uri.EMPTY
-//        private const val TRACK_LENGTH = 300000L
-//    }
-//
-//
-//}
+package com.sebastianvm.musicplayer.ui.player
+
+import com.sebastianvm.musicplayer.database.entities.C
+import com.sebastianvm.musicplayer.repository.playback.MediaItemMetadata
+import com.sebastianvm.musicplayer.repository.playback.PlaybackManager
+import com.sebastianvm.musicplayer.repository.playback.PlaybackState
+import com.sebastianvm.musicplayer.util.BaseTest
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.advanceUntilIdle
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MusicPlayerViewModelTest : BaseTest() {
+    private lateinit var playbackManager: PlaybackManager
+    private val playbackState: MutableStateFlow<PlaybackState> = MutableStateFlow(
+        PlaybackState(
+            mediaItemMetadata = MediaItemMetadata(
+                title = C.TRACK_ARGENTINA,
+                artists = C.ARTIST_ANA,
+                trackDurationMs = TRACK_DURATION,
+                artworkUri = C.IMAGE_URI_1
+            ), currentPlayTimeMs = 0, isPlaying = false
+        )
+    )
+
+
+    @Before
+    fun setUp() {
+        playbackManager = mockk(relaxUnitFun = true) {
+            every { this@mockk.playbackState } returns this@MusicPlayerViewModelTest.playbackState
+        }
+    }
+
+    private fun generateViewModel(isPlaying: Boolean = false): MusicPlayerViewModel {
+        return MusicPlayerViewModel(
+            playbackManager = playbackManager,
+            initialState = MusicPlayerState(
+                isPlaying = isPlaying,
+                trackName = null,
+                artists = null,
+                trackLengthMs = null,
+                currentPlaybackTimeMs = null,
+                trackArt = "",
+            )
+        )
+    }
+
+    @Test
+    fun `init sets initial state and listens to change in playback state`() =
+        testScope.runReliableTest {
+            with(generateViewModel()) {
+                advanceUntilIdle()
+                assertEquals(
+                    MusicPlayerState(
+                        trackName = C.TRACK_ARGENTINA,
+                        artists = C.ARTIST_ANA,
+                        trackArt = C.IMAGE_URI_1,
+                        trackLengthMs = TRACK_DURATION,
+                        isPlaying = false,
+                        currentPlaybackTimeMs = 0,
+                    ),
+                    state.value
+                )
+                playbackState.value = playbackState.value.copy(isPlaying = true)
+                advanceUntilIdle()
+                assertEquals(
+                    MusicPlayerState(
+                        trackName = C.TRACK_ARGENTINA,
+                        artists = C.ARTIST_ANA,
+                        trackArt = C.IMAGE_URI_1,
+                        trackLengthMs = TRACK_DURATION,
+                        isPlaying = true,
+                        currentPlaybackTimeMs = 0,
+                    ),
+                    state.value
+                )
+            }
+        }
+
+
+    @Test
+    fun `PlayToggled while paused triggers playback`() {
+        with(generateViewModel()) {
+            handle(MusicPlayerUserAction.PlayToggled)
+            verify { playbackManager.play() }
+        }
+    }
+
+    @Test
+    fun `PlayToggled while playing pauses playback`() {
+        with(generateViewModel(isPlaying = true)) {
+            handle(MusicPlayerUserAction.PlayToggled)
+            verify { playbackManager.pause() }
+        }
+    }
+
+
+    @Test
+    fun `PreviousButtonClicked triggers prev call`() {
+        with(generateViewModel()) {
+            handle(MusicPlayerUserAction.PreviousButtonClicked)
+            verify { playbackManager.prev() }
+        }
+    }
+
+    @Test
+    fun `NextButtonClicked triggers next call`() {
+        with(generateViewModel()) {
+            handle(MusicPlayerUserAction.NextButtonClicked)
+            verify { playbackManager.next() }
+        }
+    }
+
+    @Test
+    fun `onProgressTapped triggers seekToTrackPosition call`() = testScope.runReliableTest {
+        with(generateViewModel()) {
+            advanceUntilIdle()
+            handle(MusicPlayerUserAction.ProgressBarClicked(50))
+            verify { playbackManager.seekToTrackPosition(TRACK_DURATION / 2) }
+        }
+    }
+
+    companion object {
+        private const val TRACK_DURATION = 180000L
+    }
+
+
+}
