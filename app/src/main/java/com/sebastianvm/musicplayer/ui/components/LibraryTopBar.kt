c958056d30327a5158257f96395ea5cbf13e45e6
sebvil <>
2022-10-25 21:40:07 -0500

    upgrade libs

--- app/src/main/java/com/sebastianvm/musicplayer/ui/components/LibraryTopBar.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/ui/components/LibraryTopBar.kt
@@ -4,8 +4,8 @@ import android.content.res.Configuration
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -37,8 +37,12 @@ fun LibraryTopBar(title: String, delegate: LibraryTopBarDelegate, titleAlpha: Fl
 // TODO Need to handle arbitrarily long text
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LibraryTopBar(state: LibraryTopBarState, delegate: LibraryTopBarDelegate, titleAlpha: Float = 1f) {
-    SmallTopAppBar(
+fun LibraryTopBar(
+    state: LibraryTopBarState,
+    delegate: LibraryTopBarDelegate,
+    titleAlpha: Float = 1f
+) {
+    TopAppBar(
         title = {
             Text(text = state.title, modifier = Modifier.alpha(titleAlpha))
         },
