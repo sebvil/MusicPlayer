f77cef361941b1d74ef4bf152ae2c7a9ed194a02
Sebas <8863909+sebvil@users.noreply.github.com>
2022-12-20 23:00:23 -0600

    Moved LibraryTopBar to own package, handle overflow case with ellipsis (#78)
    Co-authored-by: sebvil <>

---
+++ app/src/main/java/com/sebastianvm/musicplayer/ui/components/topbar/LibraryTopBarPreview.kt
@@ -0,0 +1,20 @@
+package com.sebastianvm.musicplayer.ui.components.topbar
+
+import android.content.res.Configuration
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.sebastianvm.musicplayer.ui.util.compose.StringPreviewParameterProvider
+import com.sebastianvm.musicplayer.ui.util.compose.ThemedPreview
+
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun LibraryTopBarPreview(@PreviewParameter(StringPreviewParameterProvider::class, limit = 3) title: String) {
+    ThemedPreview {
+        LibraryTopBar(
+            state = LibraryTopBarState(title = title, hasSortButton = true),
+            delegate = object : LibraryTopBarDelegate {})
+    }
+}
--- app/src/main/java/com/sebastianvm/musicplayer/ui/util/compose/PreviewParameterProviders.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/ui/util/compose/PreviewParameterProviders.kt
@@ -2,16 +2,13 @@ package com.sebastianvm.musicplayer.ui.util.compose
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 
-class BooleanPreviewParameterProvider : PreviewParameterProvider<Boolean> {
-    override val values = sequenceOf(true, false)
-}
 
 class StringPreviewParameterProvider : PreviewParameterProvider<String> {
     override val values: Sequence<String>
-        get() = sequenceOf(
-            "Library",
-            "AllTracksUiEvent Songs",
-            "Tropipop"
-        )
+        get() = sequence {
+            for (i in 1..10) {
+                yield(PreviewUtil.randomString())
+            }
+        }
 
 }
