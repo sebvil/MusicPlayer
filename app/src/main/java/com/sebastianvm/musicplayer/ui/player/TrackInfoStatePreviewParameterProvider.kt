d9f68b36fa1f4f895477ee93dd95ad85125dbaf4
Sebas <github.somatotropically@simplelogin.co>
2023-08-15 11:26:46 -0500

    Fix build (#105)
    Co-authored-by: sebvil <>

---
+++ app/src/main/java/com/sebastianvm/musicplayer/ui/player/TrackInfoStatePreviewParameterProvider.kt
@@ -0,0 +1,25 @@
+package com.sebastianvm.musicplayer.ui.player
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.sebastianvm.musicplayer.ui.util.compose.PreviewUtil
+
+class TrackInfoStatePreviewParameterProvider : PreviewParameterProvider<TrackInfoState> {
+    override val values = sequenceOf(
+        TrackInfoState(
+            trackName = PreviewUtil.randomString(minLength = 40),
+            artists = PreviewUtil.randomString(minLength = 40)
+        ),
+        TrackInfoState(
+            trackName = PreviewUtil.randomString(maxLength = 15),
+            artists = PreviewUtil.randomString(maxLength = 15)
+        ),
+        TrackInfoState(
+            trackName = PreviewUtil.randomString(maxLength = 15),
+            artists = PreviewUtil.randomString(minLength = 40)
+        ),
+        TrackInfoState(
+            trackName = PreviewUtil.randomString(minLength = 40),
+            artists = PreviewUtil.randomString(maxLength = 15)
+        )
+    )
+}
