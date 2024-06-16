35ccc67965d8a53d52fed174635f6668c33183e3
sebvil <8863909+sebvil@users.noreply.github.com>
2024-06-15 18:51:18 -0700

    done with UI

--- app/src/main/java/com/sebastianvm/musicplayer/features/album/details/AlbumDetailsUiComponent.kt
+++ app/src/main/java/com/sebastianvm/musicplayer/features/album/details/AlbumDetailsUiComponent.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
@@ -36,17 +35,18 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.layout.layout
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.coerceAtMost
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastMap
+import androidx.compose.ui.util.fastMaxBy
 import com.sebastianvm.musicplayer.designsystem.components.Text
 import com.sebastianvm.musicplayer.designsystem.components.TrackRow
 import com.sebastianvm.musicplayer.di.Dependencies
@@ -135,13 +135,19 @@ fun AlbumDetails(
         object : NestedScrollConnection {
             override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
                 if (available.y > 0f) return Offset.Zero
-
+                if (!listState.canScrollForward) return Offset.Zero
                 val oldSize = size
                 size = (available.y + size).coerceAtLeast(minSize)
                 return if (oldSize != size) {
                     available.copy(x = 0f)
                 } else {
-                    Offset.Zero
+                    val oldOffset = offset
+                    offset = (available.y + offset).coerceIn(topBarHeight - fullHeaderHeight, 0f)
+                    if (oldOffset != offset) {
+                        available.copy(x = 0f)
+                    } else {
+                        Offset.Zero
+                    }
                 }
             }
 
@@ -150,93 +156,126 @@ fun AlbumDetails(
                 available: Offset,
                 source: NestedScrollSource
             ): Offset {
-                offset += consumed.y
-                if (available.y < 0f || consumed.y < 0f) {
-                    val oldSize = size
-                    size = (size + consumed.y).coerceAtLeast(minSize)
-                    return Offset(x = 0f, y = size - oldSize)
-                }
-
-                if (consumed.y == 0f && available.y > 0) {
-                    offset = 0f
-                }
-
-                if (available.y > 0f) {
+                if (!listState.canScrollForward) return Offset.Zero
+                return if (offset == 0f) {
                     val oldSize = size
                     size = (size + available.y).coerceAtMost(maxSize)
-                    return Offset(x = 0f, y = size - oldSize)
+                    if (oldSize != size) {
+                        available.copy(x = 0f)
+                    } else {
+                        Offset.Zero
+                    }
+                } else {
+                    val oldOffset = offset
+                    offset = (available.y + offset).coerceAtMost(0f)
+                    if (oldOffset != offset) {
+                        available.copy(x = 0f)
+                    } else {
+                        Offset.Zero
+                    }
                 }
-                return Offset.Zero
             }
         }
     }
 
     val animatedSize = animateFloatAsState(targetValue = size, label = "size")
+    val animatedOffset = animateFloatAsState(targetValue = offset, label = "size")
 
-    Box(modifier = modifier.nestedScroll(nestedScrollConnection)) {
-        TopBar(
-            title = state.albumName,
-            alpha = topBarAlpha,
-            onSizeChange = { topBarHeight = it.toFloat() },
-            onBackButtonClick = { handle(AlbumDetailsUserAction.BackClicked) },
-        )
-
-        Column(
-            modifier =
-                Modifier.fillMaxWidth()
-                    .offset { IntOffset(x = 0, y = offset.roundToInt()) }
-                    .onSizeChanged { size -> fullHeaderHeight = size.height.toFloat() }
-                    .padding(top = WindowInsets.systemBars.asPaddingValues().calculateTopPadding()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            MediaArtImage(
-                artworkUri = state.imageUri,
-                modifier = Modifier.stateSize(animatedSize).alpha(imageAlpha),
-            )
-
-            Column(modifier = Modifier.padding(horizontal = 16.dp).padding(bottom = 12.dp)) {
-                Text(
-                    text = state.albumName,
-                    style =
-                        MaterialTheme.typography.headlineMedium.copy(
-                            fontWeight = FontWeight.Medium
-                        ),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.alpha(1 - topBarAlpha),
-                )
-
-                state.artists?.let {
-                    Text(
-                        text = it,
-                        style =
-                            MaterialTheme.typography.titleLarge.copy(
-                                fontWeight = FontWeight.Medium
-                            ),
-                        textAlign = TextAlign.Start,
-                        modifier = Modifier.fillMaxWidth().alpha(1 - topBarAlpha),
+    SubcomposeLayout(modifier = modifier.nestedScroll(nestedScrollConnection)) { constraints ->
+        val topBarPlaceables =
+            subcompose(AlbumDetailsContent.TopBar) {
+                    TopBar(
+                        title = state.albumName,
+                        alpha = topBarAlpha,
+                        onBackButtonClick = { handle(AlbumDetailsUserAction.BackClicked) },
                     )
                 }
-            }
-        }
+                .fastMap { it.measure(constraints) }
 
-        when (state) {
-            is AlbumDetailsState.Loading -> {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        topBarHeight = topBarPlaceables.fastMaxBy { it.height }?.height?.toFloat() ?: 0f
+
+        val headerPlaceables =
+            subcompose(AlbumDetailsContent.Header) {
+                    Column(
+                        modifier =
+                            Modifier.fillMaxWidth()
+                                .padding(
+                                    top =
+                                        WindowInsets.systemBars
+                                            .asPaddingValues()
+                                            .calculateTopPadding()
+                                ),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        MediaArtImage(
+                            artworkUri = state.imageUri,
+                            modifier = Modifier.stateSize(animatedSize).alpha(imageAlpha),
+                        )
+
+                        Column(
+                            modifier = Modifier.padding(horizontal = 16.dp).padding(bottom = 12.dp)
+                        ) {
+                            Text(
+                                text = state.albumName,
+                                style =
+                                    MaterialTheme.typography.headlineMedium.copy(
+                                        fontWeight = FontWeight.Medium
+                                    ),
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.alpha(1 - topBarAlpha),
+                            )
+
+                            state.artists?.let {
+                                Text(
+                                    text = it,
+                                    style =
+                                        MaterialTheme.typography.titleLarge.copy(
+                                            fontWeight = FontWeight.Medium
+                                        ),
+                                    textAlign = TextAlign.Start,
+                                    modifier = Modifier.fillMaxWidth().alpha(1 - topBarAlpha),
+                                )
+                            }
+                        }
+                    }
                 }
-            }
-            is AlbumDetailsState.Data -> {
-                AlbumDetails(
-                    state = state,
-                    handle = handle,
-                    contentPadding =
-                        PaddingValues(
-                            top = with(density) { fullHeaderHeight.toDp().coerceAtLeast(0.dp) },
-                            bottom = LocalPaddingValues.current.calculateBottomPadding(),
-                        ),
-                    listState = listState,
-                )
-            }
+                .fastMap { it.measure(constraints) }
+
+        fullHeaderHeight = headerPlaceables.fastMaxBy { it.height }?.height?.toFloat() ?: 0f
+
+        val contentPlaceables =
+            subcompose(AlbumDetailsContent.Content) {
+                    when (state) {
+                        is AlbumDetailsState.Loading -> {
+                            Box(modifier = Modifier.fillMaxSize()) {
+                                CircularProgressIndicator(
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                        }
+                        is AlbumDetailsState.Data -> {
+                            AlbumDetails(
+                                state = state,
+                                handle = handle,
+                                contentPadding =
+                                    PaddingValues(
+                                        bottom =
+                                            (LocalPaddingValues.current.calculateBottomPadding() +
+                                                    animatedOffset.value.toDp())
+                                                .coerceAtLeast(0.dp),
+                                        top = fullHeaderHeight.toDp()
+                                    ),
+                                listState = listState,
+                            )
+                        }
+                    }
+                }
+                .fastMap { it.measure(constraints) }
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            topBarPlaceables.fastMap { it.place(0, 0) }
+            headerPlaceables.fastMap { it.place(0, animatedOffset.value.roundToInt()) }
+            contentPlaceables.fastMap { it.place(0, animatedOffset.value.roundToInt()) }
         }
     }
 }
@@ -302,3 +341,9 @@ private fun Modifier.stateSize(size: State<Float>) =
             layout(placeable.width, placeable.height) { placeable.placeRelative(0, 0) }
         }
     )
+
+private enum class AlbumDetailsContent {
+    TopBar,
+    Header,
+    Content
+}
